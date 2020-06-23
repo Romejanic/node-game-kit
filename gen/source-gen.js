b884fc624c7cf44b5c2ddf3894508e998f3bd368
Jack Davenport <jack.davenport5011@gmail.com>
2020-06-23 17:55:08 +1000

    Added parsing support for methods

--- gen/source-gen.js
+++ gen/source-gen.js
@@ -12,6 +12,24 @@ module.exports = async function(path, prefix) {
             let trimmed = line.substring("#define ".length);
             trimmed = trimmed.substring(0, trimmed.indexOf(" "));
             if(trimmed.length > 0) consts.push(trimmed);
+        } else if(line.startsWith(`${prefix}API `)) {
+            let trimmed = line.substring(`${prefix}API `.length);
+            let header = trimmed.substring(0, trimmed.indexOf("("));
+            let returnType = header.substring(0, header.lastIndexOf(" "));
+            let functionName = header.substring(header.lastIndexOf(" ")+1);
+            methods[functionName] = {
+                returnType,
+                arguments: []
+            };
+            let arguments = trimmed.substring(trimmed.indexOf("(")+1,trimmed.indexOf(")")).split(", ");
+            if(arguments == "void") continue; // skip no arguments
+            for(let argStr of arguments) {
+                let type = argStr.substring(0, argStr.lastIndexOf(" "));
+                let name = argStr.substring(argStr.lastIndexOf(" ")+1);
+                methods[functionName].arguments.push({
+                    type, name
+                });
+            }
         }
     }
     // debug output
